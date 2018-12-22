eb8fff191499eb135eac034646a3da770d100f25
Himalay <contact@himalay.com.np>
2018-12-22 22:34:47 +0545

    Add express-graphql middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import expressGraphQL from 'express-graphql'
+
+import schema from './schema'
 
 // initialize configuration
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use('/graphql', expressGraphQL({ schema, graphiql: true }))
 
 app.get('/', (req, res) => {
   res.json({ hello: 'world' })
